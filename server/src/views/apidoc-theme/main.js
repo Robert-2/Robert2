60dcdef133eb1c00e475532556118eac7917bc56
Donovan <donovan@pulsanova.com>
2020-11-13 12:20:02 +0100

    Supprime la documentation d'API (#58)

--- server/src/views/apidoc-theme/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $("table:not(.table)").addClass('table table-bordered');
-
-    $.get('version.txt', function(versionContent) {
-        $('#version-number').text(versionContent);
-    })
-});
