711b773fd441e0c37085a28ada0ff59a4d9f1d26
Donovan Lambert <donovan@pulsanova.com>
2020-11-13 17:40:08 +0100

    Supprime la création automatique d'une PWA, pas idéale en l'état

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,13 +2,11 @@ module.exports = {
   runtimeCompiler: true,
   productionSourceMap: false,
   filenameHashing: false,
-  pwa: {
-    name: 'Robert2',
-    themeColor: '#e14406',
-    msTileColor: '#ffffff',
-  },
   chainWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
+      config.plugins.delete('html');
+      config.plugins.delete('preload');
+      config.plugins.delete('prefetch');
       config.plugins.delete('friendly-errors');
     }
   },
