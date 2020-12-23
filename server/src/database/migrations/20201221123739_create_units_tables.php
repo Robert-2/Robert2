61c5499b827a585b0e541b725baa9bfcc12816d9
Donovan Lambert <donovan@pulsanova.com>
2020-12-23 14:13:47 +0100

    Améliore la migration pour la prise en charge unitaire du matériel

--- server/src/database/migrations/20201221123739_create_units_tables.php
+++ server/src/database/migrations/20201221123739_create_units_tables.php
@@ -22,7 +22,7 @@ class CreateUnitsTables extends AbstractMigration
             ->addColumn('is_broken', 'boolean', ['default' => false])
             ->addColumn('created_at', 'datetime', ['null' => true])
             ->addColumn('updated_at', 'datetime', ['null' => true])
-            ->addIndex(['serial_number'], ['unique' => true])
+            ->addIndex(['material_id', 'serial_number'], ['unique' => true])
             ->addForeignKey('material_id', 'materials', 'id', [
                 'delete' => 'CASCADE',
                 'update' => 'NO_ACTION',
@@ -36,6 +36,7 @@ class CreateUnitsTables extends AbstractMigration
             ->addColumn('material_unit_id', 'integer')
             ->addIndex(['event_material_id'])
             ->addIndex(['material_unit_id'])
+            ->addIndex(['event_material_id', 'material_unit_id'], ['unique' => true])
             ->addForeignKey('event_material_id', 'event_materials', 'id', [
                 'delete' => 'CASCADE',
                 'update' => 'NO_ACTION',
@@ -51,6 +52,14 @@ class CreateUnitsTables extends AbstractMigration
 
     public function down()
     {
+        $park = $this->fetchRow('SELECT id FROM parks ORDER BY id LIMIT 1');
+        if ($park) {
+            $this->execute(sprintf("UPDATE `materials` SET `park_id` = %d WHERE `park_id` IS NULL", $park['id']));
+        } else {
+            $this->execute("DELETE FROM `materials` WHERE `park_id` IS NULL");
+        }
+        $this->execute("UPDATE `materials` SET `stock_quantity` = 0 WHERE `stock_quantity` IS NULL");
+
         $table = $this->table('materials');
         $table
             ->changeColumn('park_id', 'integer')
