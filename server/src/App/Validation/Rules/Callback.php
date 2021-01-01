37149e4e202ca2adba2f4415c27d0d0f96f237b5
Donovan Lambert <donovan@pulsanova.com>
2021-01-01 16:14:05 +0100

    Améliore (encore) la validation

--- server/src/App/Validation/Rules/Callback.php
+++ server/src/App/Validation/Rules/Callback.php
@@ -2,8 +2,10 @@
 
 namespace Robert2\API\Validation\Rules;
 
-use Respect\Validation\Rules\AbstractRule;
 use Respect\Validation\Exceptions\ComponentException;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Respect\Validation\Rules\AbstractRule;
+use Respect\Validation\Validator;
 
 class Callback extends AbstractRule
 {
@@ -23,7 +25,32 @@ class Callback extends AbstractRule
         $this->arguments = $arguments;
     }
 
+    public function assert($input)
+    {
+        $result = $this->_validate($input);
+        if ($result instanceof NestedValidationException) {
+            throw $result;
+        }
+
+        if ($result) {
+            return true;
+        }
+
+        throw $this->reportError($input);
+    }
+
     public function validate($input)
+    {
+        return $this->_validate($input) === true;
+    }
+
+    // ------------------------------------------------------
+    // -
+    // -    Internal methods
+    // -
+    // ------------------------------------------------------
+
+    protected function _validate($input)
     {
         $params = $this->arguments;
         array_unshift($params, $input);
@@ -31,6 +58,16 @@ class Callback extends AbstractRule
         $result = call_user_func_array($this->callback, $params);
         if (is_bool($result)) {
             return $result;
+        }
+
+        if ($result instanceof Validator) {
+            try {
+                $result->setName($this->name)->assert($input);
+            } catch (NestedValidationException $e) {
+                $this->setTemplate($e->getFullMessage());
+                return $e;
+            }
+            return true;
         }
 
         $this->setTemplate($result);
