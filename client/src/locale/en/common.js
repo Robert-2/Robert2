e5cff2920eb7f8dfac4122f482c0c5aa73b51de9
Donovan Lambert <donovan@pulsanova.com>
2021-01-24 14:35:28 +0100

    Revert "Remet en place le début de vue des unités pour les matériels"
    This reverts commit eeb33df4027a138a045196ba613fa6297d3bcfe4.

--- client/src/locale/en/common.js
+++ client/src/locale/en/common.js
@@ -109,7 +109,6 @@ export default {
   'display-not-deleted-items': "Display not deleted items",
   'created-at': "Created at:",
   'updated-at': "Updated at:",
-  'units': "Units",
 
   'event-details': "Event's details",
   'title': "Title",
