8de3814e7576480571e6e6e93cca70ef35817cbd
Donovan Lambert <donovan@pulsanova.com>
2021-02-21 12:05:52 +0100

    Ajoute les tests unitaires pour la fonction `isValidInteger`

--- client/src/utils/isValidInteger.js
+++ client/src/utils/isValidInteger.js
@@ -1,5 +1,8 @@
-const isValidInteger = (value) => (
-  !Number.isNaN(value) && Number.isInteger(parseFloat(value))
-);
+const isValidInteger = (value) => {
+  if (typeof value === 'number') {
+    return !Number.isNaN(value) && Number.isInteger(parseFloat(value));
+  }
+  return /^-?[0-9]+$/.test(value);
+};
 
 export default isValidInteger;
