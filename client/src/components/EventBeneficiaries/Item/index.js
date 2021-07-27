d2f4925f8a5069eb7dd468f2ccfa90d82cdf0d9d
Paul Maillardet <paul@pulsanova.com>
2021-07-27 11:47:17 +0200

    Adapte l'affichage des techniciens dans la fenêtre d'événement

---
+++ client/src/components/EventBeneficiaries/Item/index.js
@@ -0,0 +1,30 @@
+import './index.scss';
+
+export default {
+  name: 'EventBeneficiaryItem',
+  props: {
+    beneficiary: Object,
+  },
+  render() {
+    const { $t: __, beneficiary } = this;
+
+    return (
+      <div class="EventBeneficiaryItem">
+        <router-link
+          to={`/beneficiaries/${beneficiary.id}`}
+          title={__('action-edit')}
+        >
+          {beneficiary.full_name}
+        </router-link>
+        {beneficiary.company && (
+          <router-link
+            to={`/companies/${beneficiary.company_id}`}
+            title={__('action-edit')}
+          >
+            ({beneficiary.company.legal_name})
+          </router-link>
+        )}
+      </div>
+    );
+  },
+};
