e1de28ea233b2e1428ba0a07a53376416f4d49af
faune_go <geoffrey.olivier@faune.net>
2021-08-19 10:59:07 +0200

    Ajout des matériels liés aux événements et ajout des événements liés aux matériels (api) (#252)

--- server/src/App/Controllers/MaterialController.php
+++ server/src/App/Controllers/MaterialController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Robert2\API\Controllers;
 
 use DI\Container;
+use Illuminate\Support\Carbon;
 use Robert2\API\Config\Config;
 use Robert2\API\Controllers\Traits\Taggable;
 use Robert2\API\Controllers\Traits\WithCrud;
@@ -52,6 +53,7 @@ class MaterialController extends BaseController
         $dateForQuantities = $request->getQueryParam('dateForQuantities', null);
         $withDeleted = (bool)$request->getQueryParam('deleted', false);
         $tags = $request->getQueryParam('tags', []);
+        $withEvents = (bool)$request->getQueryParam('events', false);
 
         $options = [];
         if ($parkId) {
@@ -75,6 +77,13 @@ class MaterialController extends BaseController
             $model = $model->getAll($withDeleted);
         } else {
             $model = $model->getAllFilteredOrTagged($options, $tags, $withDeleted);
+        }
+        if ($withEvents == true) {
+            $model->with('Events', function ($query) {
+                $query->where('Events.end_date', '>=', Carbon::now());
+            })
+                ->with('Events')
+                ->get();
         }
 
         $results = $this->paginate($request, $model);
