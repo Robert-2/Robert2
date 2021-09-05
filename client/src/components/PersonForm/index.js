37821530d5acb1b0cb7eef0bfd158ee466b3289b
Donovan Lambert <donovan@pulsanova.com>
2021-09-05 13:01:46 +0200

    Refactorise le component PersonForm (2)

--- client/src/components/PersonForm/index.js
+++ client/src/components/PersonForm/index.js
@@ -1,187 +1,175 @@
 import './index.scss';
+import { computed, toRefs, onMounted } from '@vue/composition-api';
+import useI18n from '@/hooks/useI18n';
+import useRouter from '@/hooks/useRouter';
 import FormField from '@/components/FormField';
 
 // @vue/component
-export default {
-    name: 'PersonForm',
-    props: {
-        person: { type: Object, default: () => ({}) },
-        errors: { type: Object, default: () => ({}) },
-        withCompany: Boolean,
-        withReference: Boolean,
-    },
-    computed: {
-        countriesOptions() {
-            return this.$store.getters['countries/options'];
-        },
+const PersonForm = (props, { root, emit }) => {
+    // FIXME: La prop `person` ne devraot âtre mutée dans ce component...
+    const { person, errors, withReference, withCompany } = toRefs(props);
+    const countriesOptions = computed(() => root.$store.getters['countries/options']);
+    const companiesOptions = computed(() => root.$store.getters['companies/options']);
+    const { router } = useRouter();
+    const __ = useI18n();
 
-        companiesOptions() {
-            return this.$store.getters['companies/options'];
-        },
-    },
-    mounted() {
-        this.$store.dispatch('countries/fetch');
-        this.$store.dispatch('companies/fetch');
-    },
-    methods: {
-        handleSubmit(e) {
-            this.$emit('submit', e);
-        },
+    onMounted(() => {
+        root.$store.dispatch('countries/fetch');
+        root.$store.dispatch('companies/fetch');
+    });
 
-        handleChange(e) {
-            this.$emit('change', e);
-        },
+    const handleSubmit = (e) => {
+        emit('submit', e);
+    };
 
-        handleBack() {
-            this.$emit('cancel');
-            this.$router.back();
-        },
-    },
-    render() {
-        const {
-            $t: __,
-            person,
-            errors,
-            countriesOptions, // TODO: Migrate.
-            companiesOptions, // TODO: Migrate.
-            withReference,
-            withCompany,
-            handleChange,
-            handleSubmit,
-            handleBack,
-        } = this;
+    const handleChange = (e) => {
+        emit('change', e);
+    };
 
-        return (
-            <form
-                class="Form Form--fixed-actions PersonForm"
-                method="POST"
-                onSubmit={handleSubmit}
-                onChange={handleChange}
-            >
-                <section class="Form__fieldset">
-                    <h4 class="Form__fieldset__title">{__('personnal-infos')}</h4>
+    const handleBack = () => {
+        emit('cancel');
+        router.back();
+    };
+
+    return () => (
+        <form
+            class="Form Form--fixed-actions PersonForm"
+            method="POST"
+            onSubmit={handleSubmit}
+            onChange={handleChange}
+        >
+            <section class="Form__fieldset">
+                <h4 class="Form__fieldset__title">{__('personnal-infos')}</h4>
+                <FormField
+                    name="first_name"
+                    label="first-name"
+                    vModel={person.value.first_name}
+                    errors={errors.value.first_name}
+                    required
+                />
+                <FormField
+                    name="last_name"
+                    label="last-name"
+                    vModel={person.value.last_name}
+                    errors={errors.value.last_name}
+                    required
+                />
+                {withReference.value && (
                     <FormField
-                        name="first_name"
-                        label="first-name"
-                        vModel={person.first_name}
-                        errors={errors.first_name}
-                        required
+                        name="reference"
+                        label="reference"
+                        vModel={person.value.reference}
+                        errors={errors.value.reference}
                     />
-                    <FormField
-                        name="last_name"
-                        label="last-name"
-                        vModel={person.last_name}
-                        errors={errors.last_name}
-                        required
-                    />
-                    {withReference && (
-                        <FormField
-                            name="reference"
-                            label="reference"
-                            vModel={person.reference}
-                            errors={errors.reference}
-                        />
-                    )}
-                    {!withCompany && (
-                        <FormField
-                            name="nickname"
-                            label="nickname"
-                            vModel={person.nickname}
-                            errors={errors.nickname}
-                        />
-                    )}
-                </section>
-                {withCompany && (
-                    <section class="Form__fieldset">
-                        <h4 class="Form__fieldset__title">{__('company')}</h4>
-                        <div class="PersonForm__company">
-                            <FormField
-                                name="company_id"
-                                label="company"
-                                type="select"
-                                options={companiesOptions}
-                                vModel={person.company_id}
-                                errors={errors.company_id}
-                            />
-                            {!!person.company_id && (
-                                <router-link
-                                    to={`/companies/${person.company_id}`}
-                                    class="PersonForm__company__edit-btn button info"
-                                >
-                                    <i class="fas fa-edit" /> {__('page-companies.edit-btn')}
-                                </router-link>
-                            )}
-                        </div>
-                        <router-link to="/companies/new" class="PersonForm__add-company">
-                            <i class="fas fa-plus" /> {__('page-companies.create-new')}
-                        </router-link>
-                    </section>
                 )}
+                {!withCompany.value && (
+                    <FormField
+                        name="nickname"
+                        label="nickname"
+                        vModel={person.value.nickname}
+                        errors={errors.value.nickname}
+                    />
+                )}
+            </section>
+            {withCompany.value && (
                 <section class="Form__fieldset">
-                    <h4 class="Form__fieldset__title">{__('contact')}</h4>
-                    <FormField
-                        name="phone"
-                        label="phone"
-                        class="PersonForm__phone"
-                        type="tel"
-                        vModel={person.phone}
-                        errors={errors.phone}
-                    />
-                    <FormField
-                        name="email"
-                        label="email"
-                        type="email"
-                        vModel={person.email}
-                        errors={errors.email}
-                    />
-                    <FormField
-                        name="street"
-                        label="street"
-                        vModel={person.street}
-                        errors={errors.street}
-                    />
-                    <FormField
-                        name="postal_code"
-                        label="postal-code"
-                        class="PersonForm__postal-code"
-                        vModel={person.postal_code}
-                        errors={errors.postal_code}
-                    />
-                    <FormField
-                        name="locality"
-                        label="city"
-                        vModel={person.locality}
-                        errors={errors.locality}
-                    />
-                    <FormField
-                        name="country_id"
-                        label="country"
-                        type="select"
-                        options={countriesOptions}
-                        vModel={person.country_id}
-                        errors={errors.country_id}
-                    />
+                    <h4 class="Form__fieldset__title">{__('company')}</h4>
+                    <div class="PersonForm__company">
+                        <FormField
+                            name="company_id"
+                            label="company"
+                            type="select"
+                            options={companiesOptions.value}
+                            vModel={person.value.company_id}
+                            errors={errors.value.company_id}
+                        />
+                        {!!person.value.company_id && (
+                            <router-link
+                                to={`/companies/${person.value.company_id}`}
+                                class="PersonForm__company__edit-btn button info"
+                            >
+                                <i class="fas fa-edit" /> {__('page-companies.edit-btn')}
+                            </router-link>
+                        )}
+                    </div>
+                    <router-link to="/companies/new" class="PersonForm__add-company">
+                        <i class="fas fa-plus" /> {__('page-companies.create-new')}
+                    </router-link>
                 </section>
-                <section class="Form__fieldset">
-                    <h4 class="Form__fieldset__title">{__('other-infos')}</h4>
-                    <FormField
-                        label="notes"
-                        name="note"
-                        type="textarea"
-                        class="PersonForm__notes"
-                        vModel={person.note}
-                        errors={errors.note}
-                    />
-                </section>
-                <section class="Form__actions">
-                    <button class="Form__actions__save success" type="submit">
-                        <i class="fas fa-save" /> {__('save')}
-                    </button>
-                    <button type="button" onClick={handleBack}>
-                        <i class="fas fa-ban" /> {__('cancel')}
-                    </button>
-                </section>
-            </form>
-        );
-    },
+            )}
+            <section class="Form__fieldset">
+                <h4 class="Form__fieldset__title">{__('contact')}</h4>
+                <FormField
+                    name="phone"
+                    label="phone"
+                    class="PersonForm__phone"
+                    type="tel"
+                    vModel={person.value.phone}
+                    errors={errors.value.phone}
+                />
+                <FormField
+                    name="email"
+                    label="email"
+                    type="email"
+                    vModel={person.value.email}
+                    errors={errors.value.email}
+                />
+                <FormField
+                    name="street"
+                    label="street"
+                    vModel={person.value.street}
+                    errors={errors.value.street}
+                />
+                <FormField
+                    name="postal_code"
+                    label="postal-code"
+                    class="PersonForm__postal-code"
+                    vModel={person.value.postal_code}
+                    errors={errors.value.postal_code}
+                />
+                <FormField
+                    name="locality"
+                    label="city"
+                    vModel={person.value.locality}
+                    errors={errors.value.locality}
+                />
+                <FormField
+                    name="country_id"
+                    label="country"
+                    type="select"
+                    options={countriesOptions.value}
+                    vModel={person.value.country_id}
+                    errors={errors.value.country_id}
+                />
+            </section>
+            <section class="Form__fieldset">
+                <h4 class="Form__fieldset__title">{__('other-infos')}</h4>
+                <FormField
+                    label="notes"
+                    name="note"
+                    type="textarea"
+                    class="PersonForm__notes"
+                    vModel={person.value.note}
+                    errors={errors.value.note}
+                />
+            </section>
+            <section class="Form__actions">
+                <button class="Form__actions__save success" type="submit">
+                    <i class="fas fa-save" /> {__('save')}
+                </button>
+                <button type="button" onClick={handleBack}>
+                    <i class="fas fa-ban" /> {__('cancel')}
+                </button>
+            </section>
+        </form>
+    );
 };
+
+PersonForm.props = {
+    person: { type: Object, default: () => ({}) },
+    errors: { type: Object, default: () => ({}) },
+    withCompany: Boolean,
+    withReference: Boolean,
+};
+
+export default PersonForm;
