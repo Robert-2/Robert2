2651b22e0bd5c837c4a98285c11bba00b33cdbe2
Paul Maillardet <paul@pulsanova.com>
2021-09-06 18:51:45 +0200

    OSS => Premium

--- client/src/pages/Calendar/_utils.js
+++ client/src/pages/Calendar/_utils.js
@@ -54,7 +54,13 @@ const formatEvent = (dataEvent, translate) => {
 
     let techniciansText = '';
     if (technicians.length > 0) {
-        const techniciansNames = technicians.map(({ technician }) => technician.full_name);
+        const techniciansNames = technicians
+            .filter((eventTechnician, index, self) => (
+                self.findIndex(
+                    ({ technician }) => (technician.id === eventTechnician.technician.id),
+                ) === index
+            ))
+            .map(({ technician }) => technician.full_name);
         techniciansText = withIcon(
             'people-carry',
             `${translate('with')} ${techniciansNames.join(', ')}`,
