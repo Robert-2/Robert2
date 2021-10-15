b9207821c6a0767b65f0d8a14b1c9ce761301c97
Paul Maillardet <paul@pulsanova.com>
2021-10-15 17:16:58 +0200

    Améliore le typage des entités d'API

--- client/src/stores/api/persons.ts
+++ client/src/stores/api/persons.ts
@@ -1,30 +1,37 @@
 /* eslint-disable babel/camelcase */
 
+import type { Country } from '@/stores/api/countries';
+import type { Company } from '@/stores/api/companies';
+
 //
-// - types
+// - Types
 //
 
 export type Person = {
     id: number,
-    user_id: number | null,
     first_name: string,
     full_name: string,
     last_name: string,
     nickname: string | null,
     reference: string | null,
-    company_id: number | null,
-    country_id: number | null,
     email: string | null,
-    company: null,
-    locality: string | null,
+    phone: string | null,
+    company_id: number | null,
+    company: Company | null,
     street: string | null,
     postal_code: string | null,
-    country: null,
-    phone: string | null,
+    locality: string | null,
+    country_id: number | null,
+    country: Country | null,
     note: string | null,
+    user_id: number | null,
     created_at: string,
     updated_at: string,
     deleted_at: string | null,
+};
+
+export type PersonWithEventPivot = Person & {
+    pivot: { event_id: number, person_id: number },
 };
 
 /* eslint-enable babel/camelcase */
