c508c9ace203078c70196a36ada1cf8dab609e4a
Donovan Lambert <donovan@pulsanova.com>
2022-02-05 10:15:09 +0100

    Convertit le markup de la page de profil utilisateur en JSX

--- client/src/pages/UserProfile/index.js
+++ client/src/pages/UserProfile/index.js
@@ -58,6 +58,34 @@ export default {
         this.fetch();
     },
     methods: {
+        // ------------------------------------------------------
+        // -
+        // -    Handlers
+        // -
+        // ------------------------------------------------------
+
+        handleSave(e) {
+            e.preventDefault();
+
+            this.save();
+        },
+
+        handleTogglePasswordEdit(e) {
+            e.preventDefault();
+
+            this.isPasswordEdit = !this.isPasswordEdit;
+            if (!this.isPasswordEdit) {
+                this.user.password = '';
+                this.user.password_confirmation = '';
+            }
+        },
+
+        // ------------------------------------------------------
+        // -
+        // -    Internal methods
+        // -
+        // ------------------------------------------------------
+
         async fetch() {
             const { id } = this.$store.state.auth.user;
             const { resource } = this.$route.meta;
@@ -141,14 +169,154 @@ export default {
 
             this.user = { ...data, person };
         },
+    },
+    render() {
+        const {
+            $t: __,
+            help,
+            error,
+            isLoading,
+            user,
+            groupId,
+            errors,
+            handleSave,
+            handleTogglePasswordEdit,
+            isPasswordEdit,
+        } = this;
 
-        togglePasswordEdit(e) {
-            e.preventDefault();
-            this.isPasswordEdit = !this.isPasswordEdit;
-            if (!this.isPasswordEdit) {
-                this.user.password = '';
-                this.user.password_confirmation = '';
-            }
-        },
+        const className = ['content__main-view', 'UserProfile', {
+            'UserProfile--password-edit': isPasswordEdit,
+        }];
+
+        return (
+            <div class="content">
+                <div class={className}>
+                    <h3 class="UserProfile__title">
+                        {__('page-profile.you-are-group', { group: __(groupId) })}
+                    </h3>
+                    <div class="UserProfile__content">
+                        <form class="Form" method="POST" onSubmit={handleSave}>
+                            <section class="Form__fieldset">
+                                <h4 class="Form__fieldset__title">{__('connexion-infos')}</h4>
+                                <FormField
+                                    vModel={user.pseudo}
+                                    name="pseudo"
+                                    label="pseudo"
+                                    required
+                                    errors={errors.pseudo}
+                                />
+                                <FormField
+                                    vModel={user.email}
+                                    name="email"
+                                    label="email"
+                                    type="email"
+                                    required
+                                    errors={errors.email}
+                                />
+                            </section>
+                            {!isPasswordEdit && (
+                                <a
+                                    role="button"
+                                    class="UserProfile__password-edit-toggle"
+                                    onClick={handleTogglePasswordEdit}
+                                >
+                                    {__('page-profile.edit-password')}
+                                </a>
+                            )}
+                            <section class="Form__fieldset UserProfile__section-password">
+                                <h4 class="Form__fieldset__title">
+                                    <a
+                                        role="button"
+                                        class="UserProfile__password-edit-cancel"
+                                        onClick={handleTogglePasswordEdit}
+                                    >
+                                        {__('cancel')}
+                                    </a>
+                                    {__('page-profile.edit-password')}
+                                </h4>
+                                <FormField
+                                    vModel={user.password}
+                                    name="password"
+                                    label="password"
+                                    type="password"
+                                    errors={errors.password}
+                                />
+                                <FormField
+                                    vModel={user.password_confirmation}
+                                    name="passwordConfirmation"
+                                    label="page-profile.password-confirmation"
+                                    type="password"
+                                />
+                            </section>
+                            <section class="Form__fieldset UserProfile__section-infos">
+                                <h4 class="Form__fieldset__title">{__('personnal-infos')}</h4>
+                                <FormField
+                                    vModel={user.person.first_name}
+                                    name="first_name"
+                                    label="first-name"
+                                    errors={errors.first_name}
+                                />
+                                <FormField
+                                    vModel={user.person.last_name}
+                                    name="last_name"
+                                    label="last-name"
+                                    errors={errors.last_name}
+                                />
+                                <FormField
+                                    vModel={user.person.nickname}
+                                    name="nickname"
+                                    label="nickname"
+                                    errors={errors.nickname}
+                                />
+                                <FormField
+                                    vModel={user.person.phone}
+                                    name="phone"
+                                    label="phone"
+                                    type="tel"
+                                    errors={errors.phone}
+                                />
+                                <FormField
+                                    vModel={user.person.street}
+                                    name="street"
+                                    label="street"
+                                    errors={errors.street}
+                                />
+                                <FormField
+                                    vModel={user.person.postal_code}
+                                    name="postal_code"
+                                    label="postal-code"
+                                    class="UserProfile__postal-code"
+                                    errors={errors.postal_code}
+                                />
+                                <FormField
+                                    vModel={user.person.locality}
+                                    name="locality"
+                                    label="city"
+                                    errors={errors.locality}
+                                />
+                            </section>
+                            <section class="Form__actions">
+                                <button class="Form__actions__save success" type="submit">
+                                    {__('save')}
+                                </button>
+                            </section>
+                        </form>
+                        <div class="UserProfile__extras">
+                            <Help message={help} error={error} isLoading={isLoading} />
+                            <div class="UserProfile__extras__buttons">
+                                <router-link to="/user-settings" custom>
+                                    {({ navigate }) => (
+                                        <button type="button" onClick={navigate} class="info">
+                                            <i class="fas fa-cogs" />
+                                            {__('your-settings')}
+                                        </button>
+                                    )}
+                                </router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     },
 };
