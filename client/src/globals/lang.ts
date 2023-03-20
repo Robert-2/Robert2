c12b56ed45061317293f1ba6fc29bbcf95dd0955
Paul Maillardet <paul@pulsanova.com>
2023-03-20 11:21:25 +0100

    Premium ⇒ OSS

---
+++ client/src/globals/lang.ts
@@ -0,0 +1,32 @@
+import config from '@/globals/config';
+
+const LOCALES_MAP: Record<string, string> = {
+    fr: 'fr',
+    en: 'en-gb',
+};
+
+/**
+ * Permet de récupérer la langue par défaut de l'application.
+ *
+ * @returns - La par défaut de l'application.
+ */
+export const getDefaultLang = (): string => config.defaultLang;
+
+/**
+ * Permet de récupérer la langue actuelle.
+ *
+ * @returns - La langue actuelle.
+ */
+export const getLang = (): string => (
+    localStorage.getItem('userLocale') ?? getDefaultLang()
+);
+
+/**
+ * Permet de récupérer la locale actuelle.
+ *
+ * @returns - La locale actuelle.
+ */
+export const getLocale = (): string => {
+    const lang = getLang();
+    return LOCALES_MAP[lang] ?? lang;
+};
